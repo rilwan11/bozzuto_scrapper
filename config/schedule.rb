222df6c4b7d6001a8ed8f0b8925fc3774a0548a7
Alexander Bezobchuk <alex@alexandersmbp2.home>
2014-03-16 12:41:44 -0400

    Added Notifier class and updated some methods

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,4 @@
+
 every 2.hours do
   command "cd #{path} && ENV=#{ENV['ENV']} /usr/local/bin/bundle exec rake scrapper:notify"
 end
--- lib/allegro_scrapper.rb
+++ lib/allegro_scrapper.rb
@@ -1,8 +1,10 @@
 require 'logbert'
+require 'base64'
+require 'csv'
 
 LOG = Logbert[self]
 
-module Appartment
+module Apartment
   class Scrapper
 
   	attr_accessor :agent, :search_url, :page
@@ -20,58 +22,112 @@ module Appartment
     	@page = agent.get(search_url)
     end
 
-    def get_price_range(entry)
+    def self.get_number_available(entry)
+    	raw_text = entry.search(".//td[@class='button']/span").text
+    	available = raw_text.match(/^[o|O]nly\s?(\d)\s?[l|L]eft$/)[1]
+    	return available
+    end
+
+    def self.get_price_range(entry)
     	price = entry.search("td[class='price']").text rescue "Could not find price :-("
     	return price
     end
 
-    def get_sq_ft(entry)
+    def self.get_sq_ft(entry)
     	space = entry.search("td[class='sqft']").text rescue "Could not find sq. ft. :-("
     	return space
     end
 
-    def get_style(entry)
+    def self.get_style(entry)
     	style = entry.search("td[class='floorPlan']").text rescue "Could not find style :-("
     	return style
     end
 
-    def get_bed(entry)
+    def self.get_bed(entry)
     	bed_count = entry.search("td[class='bed']").text rescue "Could not find bed count :-("
     	return bed_count
     end
 
-    def get_bath(entry)
+    def self.get_bath(entry)
     	bath_count = entry.search("td[class='bath']").text rescue "Could not find bath count :-("
     	return bath_count
     end
 
-    def get_availabilities
+    def get_all_availability
     	# Hopefully this DOM element is consistent is does not change each time you visit the site lol
     	table 	= page.search(".//table[@id='ctl00_contentMain_uaGrid']/tbody")
     	entries = table.search("tr")
-
-    	a = entries.select{|e| e.search("td[class='button']").search("a").text == "View Availability"}
-    	LOG.info("Found #{a.count} available rooms")
+    	a = entries.select{|e| is_available?(e) }
 
     	return a
     end
 
     def get_all_availability_count
-    	return get_availabilities.count
+    	count = get_availabilities.count 
+    	LOG.info("Found #{count} available rooms")
+
+    	return count
+    end
+
+    def get_availability(filter)
+
+    	raise "Invalid argument. Must use an array!" if not filter.is_a?(Array)
+    	
+    	available = get_availabilities
+    	e = available.select{|a| filter.include? get_style(a) }
+
+    	return e
     end
 
     def get_availability_count(filter)
-    	available = get_availabilities
-
-    	e = available.select{|a| get_style(a) == filter }
-    	LOG.info("Found #{e.count} available rooms with filter: #{filter}")
-
-    	return e.count
+    	count = get_availability(filter).count
+    	LOG.info("Found #{count} available rooms with filter(s): #{filter}")
+    	
+    	return count
     end
 
     def is_available?(entry)
     	return entry.search("td[class='button']").search("a").text == "View Availability"
     end
+
+  end
+
+  class Notifier
+
+  	# Creates a CSV file with the data we need
+  	def self.create_file(data)
+
+	    curr_time = Time.now.strftime('%m_%d_%Y')
+	    log_file = "Allegro Apartment Status - #{curr_time}.csv"
+
+	    CSV.open(log_file, "wb") do |csv|
+	    	data.each do |d|
+					style 		= Apartment::Scrapper.get_style(d)
+					price 		= Apartment::Scrapper.get_price_range(d)
+					bed 			= Apartment::Scrapper.get_bed(d)
+					available = Apartment::Scrapper.get_number_available(d)
+
+					csv << ["Style: #{style}", "Price: #{price}", "Beds: #{bed}", "Number Available: #{available}"]
+	    	end
+	    end
+
+	    return log_file
+  	end
+
+
+  	def send_email(file)
+
+  		if File.exists?(file)
+	  		csv = File.open(file, "rb")
+		    options = {:file_name => file, :file_contents => Base64.encode64(csv.read)}
+
+		    # Send email here with the options
+
+		    LOG.info("Closing csv file and deleting it!")
+		    csv.close
+		    File.delete(file)
+		end
+  	end
 
   end
 end
